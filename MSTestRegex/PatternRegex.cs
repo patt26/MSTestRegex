61c48a2a03d16094563f8ac77046ff4704c217e0
patt26 <kharchepratik123@gmail.com>
2021-04-29 22:12:09 +0530

    Added: MSTest Regex

---
+++ MSTestRegex/PatternRegex.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MSTestRegex
+{
+   public class Name
+    {
+        string Name_Validator = "^[A-Z]{1}[a-z]{3,}$";
+
+        public bool ValidateName(string result)
+        {
+            return Regex.IsMatch(result, Name_Validator);
+        }
+        public bool ValidateName1(string result1)
+        {
+            return Regex.IsMatch(result1, Name_Validator);
+        }
+    }
+    public class Password
+    {
+        string PasswordValidator = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+]).{8,}$";
+
+        public bool Validate(string name)
+        {
+            return Regex.IsMatch(name, PasswordValidator);
+        }
+    }
+    public class PhoneNum
+    {
+        string PhoneNoValidator = "^[91]{2}[ ]*[0-9]{10}$";
+
+        public bool PhoneValidator(string name)
+        {
+            return Regex.IsMatch(name, PhoneNoValidator);
+        }
+
+       
+    }
+    public class EmailId
+   {
+        string EmailIdValidator = "^[a-zA-Z0-9]+([.]{1}[a-zA-Z0-9]+)?[@]{1}[a-zA-Z]+[.][A-Za-z]{3}$";
+
+        public bool ValidateEmail(string name)
+        {
+            return Regex.IsMatch(name, EmailIdValidator);
+        }
+   }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestRegex;
+using System;
+using System.Text.RegularExpressions;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class NameUnitTest1
+    {
+        [TestMethod]
+        public void FirstName()
+        {
+            Name pattern = new Name();    //Act
+            bool result = pattern.ValidateName("Pratik");  //Arrange
+            Assert.IsTrue(result);  //Assert
+        }
+        [TestMethod]
+        public void LastName()
+        {
+            Name pattern = new Name();
+            bool result = pattern.ValidateName("Kharche");
+            Assert.IsTrue(result);
+        }
+    }
+    [TestClass]
+    public class EmailIdUnitTest2
+    {
+        [TestMethod]
+        public void EmailIdChecker()
+        {
+            EmailId pattern = new EmailId();
+            bool result = pattern.ValidateEmail("kharchepratik123@gmail.com");
+            Assert.IsTrue(result);
+        }
+    }
+    [TestClass]
+    public class PasswordUnitTest3
+    {
+        [TestMethod]
+        public void PasswordChecker()
+        {
+            Password pattern = new Password();
+            bool result = pattern.Validate("Dioluty@569");
+            Assert.IsTrue(result);
+        }
+    }
+    [TestClass]
+    public class PhoneNumUnitTest4
+    {
+        [TestMethod]
+        public void PhoneNumChecker()
+        {
+            PhoneNum pattern = new PhoneNum();
+            bool result = pattern.PhoneValidator("91 9552689356");
+            Assert.IsTrue(result);
+        }
+    }
+}
